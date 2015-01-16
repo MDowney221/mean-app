7b77911a2bc6995541bf8fa7ce0c2a9bd0d6f1b3
Matt Downey <mdowney@icct.com>
2015-01-16 15:35:49 -0500

    Updated server.js to use Express.js instead of node.js

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
+// using express
+// load the express package and create our app
+var express = require('express');
+var app = express();
 
-// Using just node.js
-// get the http and filesystem modules
-var http = require('http');
-	fs = require('fs');
+// send our index.html file to the user for the home page
+app.get('/', function(req, res) {
+	res.sendfile('C:/Javascript/mean-app'  + '/index.html');
+});
 
-// create our server using the http module
-http.createServer(function(req, res) {
-
-	// write to our server. set configuration for the response
-	res.writeHead(200, {
-		'Content-Type': 'text/html',
-		'Access-Control-Allow-Origin' : '*'
-	});
-
-	// grab the index html file using fs
-	var readStream = fs.createReadStream('C:/Javascript/mean-app' + '/index.html');
-
-	// send the html file to our user
-	readStream.pipe(res);
-}).listen(1337);
-
-// tell ourselves what's happening
-console.log("Visit me at http://localhost:1337");
+// start the server
+app.listen(1337);
+console.log('1337 is the magic port');
